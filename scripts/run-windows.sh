964508dcaceccc46a34e22ff916c6ba2a14139b6
akashslypatel <akashskypatel@gmail.com>
2026-07-01 14:57:49 -0700

    fix(swig): windows build

--- scripts/run-windows.sh
+++ scripts/run-windows.sh
@@ -2679,6 +2679,13 @@ build_swig() {
   download_and_unpack_file "$repo" "$lib"
   change_dir "$src_dir/$lib"
   touch "no.autogen"
+  local automake_aux_dir
+  automake_aux_dir="$(automake --print-libdir)"
+  for automake_aux_file in compile config.guess config.sub depcomp install-sh missing ylwrap; do
+    [[ -f "Tools/config/$automake_aux_file" || ! -f "$automake_aux_dir/$automake_aux_file" ]] && continue
+    cp "$automake_aux_dir/$automake_aux_file" "Tools/config/$automake_aux_file"
+  done
+  touch "no.autoreconf"
   do_configure "--build=\"$build_triple\" \
 --host=\"$build_triple\" \
 --prefix=$dependency_install_prefix \
