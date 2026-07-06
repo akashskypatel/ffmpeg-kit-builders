299f21e2d9daac5624f9e52ef8815c094df89219
akashskypatel <akashskypatel@gmail.com>
2026-07-06 08:25:06 -0700

    fix(libzvbi): macos for both arm64 and amd64

--- scripts/run-macos.sh
+++ scripts/run-macos.sh
@@ -3732,7 +3732,9 @@ build_libzvbi() {
   change_dir "$src_dir"
   do_git_checkout "$repo" "$src_dir/$lib" "$repo_ver"
   change_dir "$src_dir/$lib"
-  export CPPFLAGS="$CPPFLAGS -Diconv=libiconv -Diconv_open=libiconv_open -Diconv_close=libiconv_close"
+  if [[ "$host_arch" != "x86_64" ]]; then
+    export CPPFLAGS="$CPPFLAGS -Diconv=libiconv -Diconv_open=libiconv_open -Diconv_close=libiconv_close"
+  fi
   export LIBS="-lpng -lz -liconv -lm"
   export LDFLAGS="$LDFLAGS"
   local ORIG_PATH=$PATH
