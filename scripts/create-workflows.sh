de90341a98a573a85a6fbafd67ef2be2b2824c46
akashslypatel <akashskypatel@gmail.com>
2026-07-08 12:58:04 -0700

    change min gnu bash version

--- scripts/create-workflows.sh
+++ scripts/create-workflows.sh
@@ -2,14 +2,14 @@
 
 # shellcheck disable=SC2317,SC2129,SC1091,SC2120,SC2035,SC2016,SC2310,SC2155,SC2154,SC2034,2250,2249,2312,2292,2207
 
-if (( BASH_VERSINFO[0] < 5 )); then
+if (( BASH_VERSINFO[0] < 4 )); then
     for bash in /opt/homebrew/bin/bash /usr/local/bin/bash; do
         if [[ -x "$bash" ]]; then
             exec "$bash" "$0" "$@"
         fi
     done
 
-    echo "GNU Bash 5+ is required." >&2
+    echo "GNU Bash 4+ is required." >&2
     exit 1
 fi
 
